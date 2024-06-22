4440dcb4c4ed174461f53fa10f65335611071bec
User <housechel@mail.ru>
2024-06-22 08:37:42 +0300

    add shadcn

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup></script>
 
 <template>
-  <aside>
-    <NuxtLink to="/">
-      <NuxtImg src="logo.jpg"></NuxtImg>
+  <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+    <NuxtLink to="/" class="mb-10 block">
+      <NuxtImg
+        src="logo.jpg"
+        alt="Logo"
+        width="100px"
+        class="mx-auto"
+      ></NuxtImg>
     </NuxtLink>
   </aside>
 </template>
